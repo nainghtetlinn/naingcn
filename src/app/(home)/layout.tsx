82aea76ea8571a8af385a61ea5cb1abfb1f8d258
nainghtetlinn <naing.work.it@gmail.com>
2025-11-04 13:00:15 +0630

    feat: init basic docs

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,20 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+export default function Layout({ children }: LayoutProps<"/">) {
+  return (
+    <HomeLayout
+      {...baseOptions()}
+      links={[
+        {
+          type: "main",
+          text: "Components",
+          url: "/docs/components",
+          active: "url",
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
